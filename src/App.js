56ac436cd04e50d02dde242ada2e4130b58ea701
leerivera <leerivera80@gmail.com>
2021-03-14 22:14:27 -0400

    card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Cards from './Components/Cards/Cards'
 
 export default function App() {
   return (
     <div className="App">
      <h1>app</h1>
+     <Cards/>
     </div>
   );
 }
